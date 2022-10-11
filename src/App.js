07528e972b9fa8a2e25489375c2493b90319f1f2
Alexander Mudji Hardjo <ahardjo@magill.local>
2022-10-11 12:00:55 +1100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from './components/Header';
+import Forecast from './components/Forecast';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header/>
+      <BrowserRouter>
+      <Routes>
+        <Route path = "/" element={<Home/>}/>
+        <Route path = "/Forecast" element={<Forecast/>}/>
+      </Routes>
+    </BrowserRouter>
     </div>
+
+    
+
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 
---
+++ src/components/Forecast.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Forecast = () => {
+    return (
+        <h4>
+            Forecast
+        </h4>
+
+    )
+}
+
+export default Forecast;
