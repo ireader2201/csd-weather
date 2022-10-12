12f5fa982f69a823f21612a07ea2c7cc9f98e95e
Alexander Mudji Hardjo <ahardjo@magill.local>
2022-10-12 12:15:13 +1100

    Consume data with AWS API

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -5,38 +5,32 @@ import { useEffect, useState } from 'react';
 
 const Cards = () => {
 
-    const url = 'https://api.weatherapi.com/v1/forecast.json?key=df858f61cf6141ab89403119222909&q=London&days=5&aqi=no&alerts=no';
+    // const url = 'https://api.weatherapi.com/v1/forecast.json?key=df858f61cf6141ab89403119222909&q=London&days=5&aqi=no&alerts=no';
+    const url = "https://hwxez3ozxi.execute-api.us-east-1.amazonaws.com/default";
     const [data, setData] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
 
     const fetchData = () => {
+        // axios.defaults.headers.get['Access-Control-Allow-Origin'] = '*';
         axios.get(url)
             .then((response)=>{
                 setIsLoading(false);
-                setData(response.data);
-                console.log(response.data);
+                setData(JSON.parse(response.data));
+                console.log(JSON.parse(response.data));
             })
     }
-
-    // const fetchData = async ()  => {
-    //     try {
-    //         const response = await axios.get(url);
-    //         console.log(response);
-    //         const data = response.data;
-    //         setData(data);
-    //     } catch(error) {
-    //         console.log(error);
-    //     }
-    // }
 
     useEffect(()=>fetchData(), [])
     
     return (
         Object.keys(data).length > 0 && 
-        <Card style={{ width: '50rem', alignItems: 'center', justifyContent: 'center'}}>
-            <Card.Img variant="top" src={"https://"+data.current.condition.icon}/>
-            <Card.Body>
-                {/* <Card.Title>{data.current.humidity}</Card.Title> */}
+        <Card style={{ width: '25rem', alignItems: 'center', justifyContent: 'center'}}>
+            <Card.Img variant="top" src={"https://"+data.current.condition.icon} style={{width:"50%", height: "50%"}}/>
+            <Card.Body >
+                <Card.Title style={{textAlign: 'center', fontSize: '30px'}}>{data.current.current_temp_C}<strong>&#8451;</strong></Card.Title>
+                <Card.Subtitle style={{textAlign: 'center', margin: '10px'}}>
+                    {data.current.location[0] + ', ' + data.current.location[1]}
+                </Card.Subtitle>
                 <Card.Text>
                 Some quick example text to build on the card title and make up the
                 bulk of the card's content. 
