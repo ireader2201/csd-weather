987f4ada8bbe0b049a53f6a8ea792d853397791b
Alexander Mudji Hardjo <ahardjo@magill.local>
2022-10-14 16:04:57 +1100

    updated search location on homepage

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -6,24 +6,88 @@ import { useEffect, useState } from 'react';
 const Cards = () => {
 
     // const url = 'https://api.weatherapi.com/v1/forecast.json?key=df858f61cf6141ab89403119222909&q=London&days=5&aqi=no&alerts=no';
-    const url = "https://hwxez3ozxi.execute-api.us-east-1.amazonaws.com/default";
+    // const url = "https://hwxez3ozxi.execute-api.us-east-1.amazonaws.com/default";
+    // const url = 'https://i2w7t3w0b6.execute-api.us-east-1.amazonaws.com/weather?city=Parramatta';
     const [data, setData] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
+    const [city, setCity] = useState('');
+    const url = 'https://i2w7t3w0b6.execute-api.us-east-1.amazonaws.com/weather?city=Parramatta';
 
+
+    // const fetchData = () => {
+    //     // axios.defaults.headers.get['Access-Control-Allow-Origin'] = '*';
+    //     axios.get(url)
+    //         .then((response)=>{
+    //             setIsLoading(false);
+    //             setData((response.data));
+    //             console.log((response.data));
+    //         })
+    // }
     const fetchData = () => {
-        // axios.defaults.headers.get['Access-Control-Allow-Origin'] = '*';
-        axios.get(url)
+        if(city===''){
+            axios.get(url)
             .then((response)=>{
+                // setLoadingStatus(isLoading);
+                // setIsLoading(false);
                 setIsLoading(false);
-                setData(JSON.parse(response.data));
-                console.log(JSON.parse(response.data));
+                // setData(JSON.parse(response.data));
+                setData(response.data);
+                console.log((response.data));
+
             })
+        } else{
+            axios.get(url2(city))
+            .then((response)=>{
+                // setIsLoading(false);
+                setIsLoading(false);
+                // setLoadingStatus(isLoading);
+
+
+
+                // setData(JSON.parse(response.data));
+                setData(response.data);
+                console.log((response.data));
+
+            })
+        }
     }
 
-    useEffect(()=>fetchData(), [])
+
+    const handleChange = event => {
+        setCity(event.target.value);
+        console.log(event.target.value);
+    };
+
+    const handleClick = event => {
+        event.preventDefault();
+        console.log(url2(city));
+        // console.log(setLoadingStatus);
+        setIsLoading(true);
+        console.log(city);
+    }
+
+    const url2 = (link) => {
+        return 'https://i2w7t3w0b6.execute-api.us-east-1.amazonaws.com/weather?city=' + link;
+    }
+    useEffect(()=>fetchData(), [isLoading])
     
     return (
         Object.keys(data).length > 0 && 
+        <div>
+            <div style={{justifyContent: 'center', alignItems: 'center', display: 'flex'}}>
+                    <span>Enter city name: </span>
+                    <input
+                        type='text'
+                        id='cityname'
+                        name='cityname'
+                        onChange= {handleChange}
+                        value = {city}
+                        // ref={inputRef}
+                        >
+                    </input>&nbsp; 
+                    <button onClick={handleClick}>Show Forecast</button>
+            </div>
+            <br/>
         <Card style={{ width: '25rem', alignItems: 'center', justifyContent: 'center'}}>
             <Card.Title style={{textAlign: 'center', marginTop: '10px'}}>{data.current.current_temp_C}<strong>&#8451;</strong></Card.Title>
             <Card.Title>{data.current.condition.text}</Card.Title>
@@ -54,6 +118,7 @@ const Cards = () => {
             </Card.Body>
             <Button variant="dark" style={{marginBottom: '10px'}} href='/Forecast'>Check Weather Forecast</Button>
         </Card>
+        </div>
     );
 }
 
--- src/components/CurrentCards.js
+++ src/components/CurrentCards.js
@@ -6,7 +6,9 @@ import { useEffect, useState } from 'react';
 const CurrentCards = () => {
 
     // const url = 'https://api.weatherapi.com/v1/forecast.json?key=df858f61cf6141ab89403119222909&q=London&days=5&aqi=no&alerts=no';
-    const url = "https://hwxez3ozxi.execute-api.us-east-1.amazonaws.com/default";
+    // const url = 'https://i2w7t3w0b6.execute-api.us-east-1.amazonaws.com/weather?city=Parramatta';
+    // const url = "https://hwxez3ozxi.execute-api.us-east-1.amazonaws.com/default";
+    const url = 'https://i2w7t3w0b6.execute-api.us-east-1.amazonaws.com/weather?city=Parramatta';
     const [data, setData] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
 
@@ -15,8 +17,8 @@ const CurrentCards = () => {
         axios.get(url)
             .then((response)=>{
                 setIsLoading(false);
-                setData(JSON.parse(response.data));
-                console.log(JSON.parse(response.data));
+                setData((response.data));
+                console.log((response.data));
             })
     }
 
